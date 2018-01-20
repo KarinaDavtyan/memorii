fd5815c6eedaf167b2cb82a3d0489699b95cf074
KarinaDavtyan <k.davtyan27@gmail.com>
2018-01-20 22:15:56 +0100

    style: Login component renamed to Submit and improved

---
+++ src/containers/Submit.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import Paper from 'material-ui/Paper';
+import {pinkA400} from 'material-ui/styles/colors';
+
+class Submit extends React.Component {
+  render () {
+    return (
+      <div className="Submit">
+        <div>
+          <p>
+            Insert pair of words you want to learn.
+          </p>
+        </div>
+        <div className="inputs">
+          <div className="firstInput">
+            <TextField
+              floatingLabelText="One"
+            />
+          </div>
+          <div className="secondInput">
+            <TextField
+              floatingLabelText="Two"
+            />
+          </div>
+        </div>
+        <RaisedButton
+          label="Send"
+          labelColor={pinkA400}
+        />
+      </div>
+    )
+  }
+}
+
+export default Submit;
