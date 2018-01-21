8afd2b3e887b0db905f840c7ac5fccb6f1c36be7
KarinaDavtyan <k.davtyan27@gmail.com>
2018-01-21 23:22:46 +0100

    feat: user logges in

--- src/containers/LogInForm.js
+++ src/containers/LogInForm.js
@@ -5,18 +5,54 @@ import {pinkA400} from 'material-ui/styles/colors';
 
 class LogInForm extends React.Component {
 
+  state = {
+    username: '',
+    password: ''
+  }
+
+  fetchUserSession = async () => {
+    let { username, password } = this.state;
+    const encoded = btoa(`${username}:${password}`);
+    fetch('http://Karina-MacBookPro.local:3000/sign-in', {
+      headers: {
+        'Authorization': `Basic ${encoded}`,
+      }
+    })
+      .then(user => user.json())
+      .then(user => {
+        console.log(user);
+        return user
+      })
+  }
+
+  handleChanges = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = () => this.fetchUserSession();
+
   render () {
     return (
       <div className="LogInForm">
         <TextField
           floatingLabelText="Username"
+          onChange={this.handleChanges}
+          name='username'
+          value={this.state.username}
         />
         <TextField
           floatingLabelText="Password"
+          onChange={this.handleChanges}
+          name='password'
+          value={this.state.password}
+          type='password'
         />
         <RaisedButton
           label="Log In"
           labelColor={pinkA400}
+          onClick={this.handleSubmit}
         />
       </div>
     )
