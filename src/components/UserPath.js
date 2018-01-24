cac960607053d4e335d6ad116798d70999dc9c8e
KarinaDavtyan <k.davtyan27@gmail.com>
2018-01-24 12:58:13 +0100

    refactor: userpath to functional component

--- src/components/UserPath.js
+++ src/components/UserPath.js
@@ -3,34 +3,36 @@ import RaisedButton from 'material-ui/RaisedButton';
 import { pinkA400 } from 'material-ui/styles/colors';
 import { Link } from 'react-router-dom';
 
-class UserPath extends React.Component {
+// class UserPath extends React.Component {
 
-  render () {
-    return (
-      <div className="UserPath">
-        <h1>
-          Are you new here?
-        </h1>
-        <div className="buttons">
-          <div className="yesButton">
-            <Link to="/register">
-              <RaisedButton
-                label="Yes"
-                labelColor={pinkA400}
-              />
-            </Link>
-          </div>
-          <div className="noButton">
-            <Link to="/login">
-              <RaisedButton
-                label="No"
-              />
-            </Link>
-          </div>
+// render () {
+const UserPath = () => {
+  return (
+    <div className="UserPath">
+      <h1>
+        Are you new here?
+      </h1>
+      <div className="buttons">
+        <div className="yesButton">
+          <Link to="/register">
+            <RaisedButton
+              label="Yes"
+              labelColor={pinkA400}
+            />
+          </Link>
+        </div>
+        <div className="noButton">
+          <Link to="/login">
+            <RaisedButton
+              label="No"
+            />
+          </Link>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+// }
+// }
 
 export default UserPath;
