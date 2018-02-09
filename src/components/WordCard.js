a38906bb06f86a74395ecf3ff86949c33a7de6e5
KarinaDavtyan <k.davtyan27@gmail.com>
2018-02-09 15:56:52 +0100

    added word card component

---
+++ src/components/WordCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class WordsCard extends React.Component {
+
+  render () {
+    return (
+      <div className='WordCard'>
+        <div className='WordCardTitle'>
+          おねがいします
+        </div>
+      </div>
+    )
+  }
+}
+
+export default WordsCard;
