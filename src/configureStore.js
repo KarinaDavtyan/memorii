3f07e49978e1dac6c897f5f7068cd12c5a48d175
KarinaDavtyan <k.davtyan27@gmail.com>
2018-02-11 15:00:10 +0100

    added promise middleware + applyMiddlewares

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,31 +1,43 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import throttle from 'lodash/throttle';
 
 import reducer from './reducers/';
 import { loadState, saveState } from './LocalStorage';
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  if (!console.group) return rawDispatch;
+const logger = (store) => (next) => {
+  if (!console.group) return next;
   return (action) => {
     console.group(action.type);
     console.group('%c prev state', 'color: gray', store.getState());
     console.group('%c action', 'color: blue', action);
-    const returnValue = rawDispatch(action);
+    const returnValue = next(action);
     console.group('%c next state', 'color: green', store.getState());
     console.groupEnd(action.type);
     return returnValue;
   }
 }
 
+const promise = (store) => (next) => (action) => {
+  if (typeof action.then === 'function') {
+    return action.then(next);
+  }
+  return next(action);
+}
 
 const configureStore = () => {
-  const persistedState = loadState();
-  const store = createStore(reducer, persistedState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  const middlewares = [promise];
 
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store)
+    middlewares.push(logger);
   }
+
+  const persistedState = loadState();
+  const store = createStore(
+    reducer,
+    persistedState,
+    applyMiddleware(...middlewares),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
   store.subscribe(throttle(() => {
     saveState(store.getState())
