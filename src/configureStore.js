d8247c361a4179351031bad109b976ff60dc5124
KarinaDavtyan <k.davtyan27@gmail.com>
2018-02-11 14:34:02 +0100

    log redux actions

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,9 +4,28 @@ import throttle from 'lodash/throttle';
 import reducer from './reducers/';
 import { loadState, saveState } from './LocalStorage';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) return rawDispatch;
+  return (action) => {
+    console.group(action.type);
+    console.group('%c prev state', 'color: gray', store.getState());
+    console.group('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.group('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  }
+}
+
+
 const configureStore = () => {
   const persistedState = loadState();
   const store = createStore(reducer, persistedState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store)
+  }
 
   store.subscribe(throttle(() => {
     saveState(store.getState())
