e5dda21c00847ad444976498fad4a08f46135d41
KarinaDavtyan <k.davtyan27@gmail.com>
2018-02-12 21:16:30 +0100

    refactor: style of words list

--- src/components/WordsList.js
+++ src/components/WordsList.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Divider from 'material-ui/Divider';
-import FlatButton from 'material-ui/FlatButton';
-import Paper from 'material-ui/Paper';
 import { ReverseSort } from '../helpers';
 
 
@@ -12,39 +9,47 @@ class WordsList extends React.Component {
   }
 
   renderWord = (first, second) => {
-    const style = {
-      padding: 5
-    };
     if (this.state.first === first) {
-      return <Paper style={style}> {second} </Paper>
+      return (
+        <div className='WordsCard'>
+          <p>
+            {second}
+          </p>
+        </div>
+      )
 
     } else {
-      return <Paper style={style}> {first} </Paper>
+      return (
+        <div className='WordsCard'>
+          <p>
+            {first}
+          </p>
+        </div>
+      )
     }
   }
   renderWordsList = () => {
     if (this.props.words) {
       let words = this.props.words.sort(ReverseSort).map(word => {
         return (
-          <div className='WordsItem' key={word._id}>
-            <div className='WordsTitle'>
-              <div className="Word"
-                onMouseEnter={() => {
-                  this.setState({
-                    first: word.firstWord
-                  })
-                }}
-              >
-                {this.renderWord(word.firstWord, word.secondWord)}
-              </div>
-              <div className='DeleteButton'>
-                <FlatButton
-                  label='DELETE'
-                  onClick={() => this.props.onDelete(word.firstWord, word.secondWord)}
-                />
-              </div>
-            </div>
-            <Divider/>
+          <div
+            className='Word'
+            key={word._id}
+            onMouseEnter={() => {
+              this.setState({
+                first: word.firstWord
+              })
+            }}
+            onMouseLeave={() => {
+              this.setState({
+                first: ''
+              })
+            }}
+            onDoubleClick={
+              () => this.props.onDelete(word.firstWord, word.secondWord)
+            }
+          >
+            {this.renderWord(word.firstWord, word.secondWord)}
           </div>
         )
       })
@@ -54,13 +59,7 @@ class WordsList extends React.Component {
 
   render () {
     return (
-      <div className='WordsList'
-        onMouseLeave={() => {
-          this.setState({
-            first: ''
-          })
-        }}
-      >
+      <div className='WordsList'>
         <h3>{this.props.selection}</h3>
         <div className='WordsContainer'>
           {this.renderWordsList()}
