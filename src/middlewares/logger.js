018dc639f2cfa6b6bf93a93456c683fcb8866a3a
KarinaDavtyan <k.davtyan27@gmail.com>
2018-02-14 12:33:34 +0100

    refactor: simplifying logger middleware

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,14 +1,6 @@
-export default (store) => (next) => {
-  if (!console.group) return next;
-  return (action) => {
-    if (action.type) {
-      console.group(action.type);
-      console.group('%c prev state', 'color: gray', store.getState());
-      console.group('%c action', 'color: blue', action);
-      const returnValue = next(action);
-      console.group('%c next state', 'color: green', store.getState());
-      console.groupEnd(action.type);
-      return returnValue;
-    }
+export default (store) => (next) => (action) => {
+  if (action.type) {
+    console.log(`%c ${action.type} `, 'background: #FFDEFF; color: #000071');
+    return next(action)
   }
 }
