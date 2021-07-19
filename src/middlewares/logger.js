8844b34d7dfadebe8add46cab2dc0c504dbfef9f
Karina Davtyan <k.davtyan27@gmail.com>
2021-07-19 11:06:38 +0200

    refactor: start renewing project in svelte

--- src/middlewares/logger.js
+++
@@ -1,7 +0,0 @@
-export default (store) => (next) => (action) => {
-  if (action.type) {
-    console.log(`%c ${action.type} `, 'background: #FFDEFF; color: #000071');
-    if (action.fullError) console.log(action.fullError);
-    return next(action)
-  }
-}
